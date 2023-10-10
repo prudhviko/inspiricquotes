cedbd62c71b27436ea3309f59f54e0f38372a188
prudhviko <prudhvikovagana@gmail.com>
2023-10-10 10:29:30 +0530

    setup

--- quotesapp/models.py
+++ quotesapp/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 class Quote(models.Model):
     text = models.TextField()
-    image = models.ImageField(upload_to='quotes/', null=True, blank=True)
+    image = models.ImageField(upload_to='inspiricquotes/', null=True, blank=True)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
+    meta_title = models.CharField(max_length=60, blank=True, null=True)
+    meta_description = models.CharField(max_length=160, blank=True,null=True)
     tags = models.ManyToManyField('Tag', related_name='quotes')
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
 
@@ -18,6 +20,8 @@ class Tag(models.Model):
         return self.name
 
 class Category(models.Model):
+    meta_title = models.CharField(max_length=60, blank=True, null=True)
+    meta_description = models.CharField(max_length=160, blank=True,null=True)
     name = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
@@ -26,11 +30,15 @@ class Category(models.Model):
 class Author(models.Model):
     name = models.CharField(max_length=200)
     bio = models.TextField(null=True, blank=True)
+    meta_title = models.CharField(max_length=60, blank=True, null=True)
+    meta_description = models.CharField(max_length=160, blank=True,null=True)
     
     def __str__(self):
         return self.name
 
 class Language(models.Model):
+    meta_title = models.CharField(max_length=60, blank=True, null=True)
+    meta_description = models.CharField(max_length=160, blank=True,null=True)
     name = models.CharField(max_length=50)
     
     def __str__(self):
--- quotesapp/views.py
+++ quotesapp/views.py
@@ -7,13 +7,12 @@ def custom_404(request, exception):
     return render(request, "404.html",status=404)
 
 def home(request):
-    return HttpResponse("Helo, World...")
-    # quotes = Quote.objects.all()
-    # paginator = Paginator(quotes, 18)
-    # page = request.GET.get('page')
-    # objects = paginator.get_page(page)
-    # context = {'objects': objects}
-    # return render(request, 'home.html', context)
+    quotes = Quote.objects.all()
+    paginator = Paginator(quotes, 18)
+    page = request.GET.get('page')
+    objects = paginator.get_page(page)
+    context = {'objects': objects}
+    return render(request, 'home.html', context)
 
 def quotes(request, id):
     current_quote = get_object_or_404(Quote, id=id)
