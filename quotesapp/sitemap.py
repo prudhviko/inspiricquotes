455410cc6ef95a185230020e949b1119243dff54
prudhviko <prudhvikovagana@gmail.com>
2023-10-30 22:15:42 +0530

    sitemap done

--- quotesapp/sitemap.py
+++ quotesapp/sitemap.py
@@ -1,6 +1,8 @@
 from django.contrib.sitemaps import Sitemap
+from django.db.models.base import Model
 from django.urls import reverse
 from .models import Quote,Category,Language,Author
+from django.utils.text import slugify
 
 
 class StaticViewSitemap(Sitemap):
@@ -28,9 +30,62 @@ class CategorySitemap(Sitemap):
     priority = 0.8
 
     def items(self):
-        return Quote.objects.all()
+        return Category.objects.all()
 
     def location(self, obj):
-        return reverse('quotes_by_categories', args=[obj.id,obj.name])
+        return reverse('quotes_by_categories', args=[obj.id,slugify(obj.name)])
+
+class DisplayQuotesCategorySitemap(Sitemap):
+    changefreq = 'Hourly'
+    priority = 0.8
+
+    def items(self):
+        return Quote.objects.all()
+    
+    def location(self, obj):
+        return reverse('display_quotes_categories', args=[obj.id,obj.category.id])
+
+
+class LanguageSitemap(Sitemap):
+    changefreq = 'Hourly'
+    priority = 0.8
+
+    def items(self):
+        return Language.objects.all()
+    
+    def location(self, obj):
+        return reverse('quotes_by_languages', args=[obj.id,slugify(obj.name)])
+
+class DisplayQuotesLanguageSitemap(Sitemap):
+    changefreq = 'Hourly'
+    priority = 0.8
+
+    def items(self):
+        return Quote.objects.all()
+    
+    def location(self, obj):
+        return reverse('display_quotes_languages', args=[obj.id,obj.language.id])
+
+class AuthorSitemap(Sitemap):
+    changefreq = 'Hourly'
+    priority = 0.8
+
+    def items(self):
+        return Author.objects.all()
+    
+    def location(self,obj):
+        return reverse('quotes_by_authors',args=[obj.id,slugify(obj.name)])
+
+class DisplayQuotesAuthorSitemap(Sitemap):
+    changefreq = 'Hourly'
+    priority = 0.8
+
+    def items(self):
+        return Quote.objects.all()
+    
+    def location(self, obj):
+        return reverse('display_quotes_authors', args=[obj.id,obj.author.id])
+
+
 
 
--- website/urls.py
+++ website/urls.py
@@ -2,13 +2,20 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from quotesapp.sitemap import StaticViewSitemap
+from quotesapp.sitemap import StaticViewSitemap,QuoteSitemap,CategorySitemap,DisplayQuotesCategorySitemap,LanguageSitemap,AuthorSitemap,DisplayQuotesLanguageSitemap,DisplayQuotesAuthorSitemap
 from django.contrib.sitemaps.views import sitemap
 
 handler404 = 'quotesapp.views.custom_404'
 
 sitemaps = {
     'static': StaticViewSitemap,
+    'quotesitemap': QuoteSitemap,
+    'categorysitemap': CategorySitemap,
+    'displayquotescategorysitemap': DisplayQuotesCategorySitemap,
+    'languagesitemap': LanguageSitemap,
+    'authorsitemap' : AuthorSitemap,
+    'displayquotesauthorsitemap': DisplayQuotesAuthorSitemap,
+    'displayquoteslanguagesitemap' : DisplayQuotesLanguageSitemap
 }
 
 urlpatterns = [
