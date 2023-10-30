fb7b07a4c6b5ca01f3b88d49224c29ad84c53280
prudhviko <prudhvikovagana@gmail.com>
2023-10-30 17:26:01 +0530

    instagram removed & twitter social share added

--- static/js/script.js
+++ static/js/script.js
@@ -8,15 +8,6 @@ function shareOnTwitter(shareUrl) {
     window.open(twitterUrl, '_blank');
 }
 
-function shareOnLinkedIn(shareUrl) {
-    const linkedinUrl = `https://www.linkedin.com/shareArticle?mini=true&url=${encodeURIComponent(shareUrl)}`;
-    window.open(linkedinUrl, '_blank');
-}
-
-function shareOnInstagram(shareUrl) {
-    const instagramUrl = `https://www.instagram.com/create/?url=${encodeURIComponent(shareUrl)}`;
-    window.open(instagramUrl, '_blank');
-}
 
 function shareOnTelegram(shareUrl,shareText) {
     const telegramUrl = `https://t.me/share/url?url=${encodeURIComponent(shareUrl)}&text=${encodeURIComponent(shareText)}`;
@@ -25,7 +16,6 @@ function shareOnTelegram(shareUrl,shareText) {
 
 function shareOnWhatsApp(shareUrl,shareText) {
     const sT = `${shareUrl}`
-    console.log(sT)
     const whatsappUrl = `https://api.whatsapp.com/send?text=${encodeURIComponent(sT)}`;
     window.open(whatsappUrl, '_blank');
 }
